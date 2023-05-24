7e79dec5682922ece15059901227b0ad6bc2076e
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-05-24 23:02:29 +0200

    added TrainsTableSeeder file

--- database/migrations/2023_05_24_180749_create_trains_table.php
+++ database/migrations/2023_05_24_180749_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 20);
+            $table->string('stazione_partenza', 20);
+            $table->string('stazione_arrivo', 20);
+            $table->timeTz('orario_partenza', $precision = 0);
+            $table->timeTz('orario_arrivo', $precision = 0);
+            $table->smallInteger('codice')->unsigned();
+            $table->tinyInteger('vagoni')->unsigned();
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(0);
             $table->timestamps();
         });
     }
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+       for($i = 0; $i < 20; $i++) {
+            $newTrain = new Train();
+            $newTrain->company = $faker->words(5, true);
+            $newTrain->departure_station = $faker->words(5, true);
+            $newTrain->arrival_station = $faker->words(5, true);
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_code = $faker->ean8();
+            $newTrain->carriages = $faker->numberBetween(1, 50);
+            $newTrain->delay = $faker->boolean();
+            $newTrain->deleted = $faker->boolean();
+            $newTrain->save();
+       }
+    }
+}
