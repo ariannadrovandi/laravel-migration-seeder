bb954ae43e38ce4e35aa3d9b7e3154c67c88fd60
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-05-25 00:04:09 +0200

    added controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 
 @section('maincontent')
-    <main>
-        <h1>Home</h1>
-    </main>
+<main>
+    <h1 class="text-center">Treni</h1>
+    @foreach ($trains as $train)
+    <ul>
+        <li>{{ $train->azienda }}</li>
+        <li>{{ $train->stazione_partenza }}</li>
+        <li>{{ $train->stazione_arrivo }}</li>
+        <li>{{ $train->orario_partenza }}</li>
+        <li>{{ $train->orario_arrivo }}</li>
+        <li>{{ $train->codice }}</li>
+        <li>{{ $train->vagoni }}</li>
+        <li>{{ $train->in_orario }}</li>
+        <li>{{ $train->cancellato }}</li>
+    </ul>
+    @endforeach
+</main>
 @endsection
